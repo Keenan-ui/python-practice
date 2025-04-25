961b3069567ad5ef6f84d718ccd4906611ce9865
Keenan Joshua <joshua.onyango@strathmore.edu>
2025-04-25 20:46:07 +0300

    Update Reverse a string.py

--- Reverse a string.py
+++ Reverse a string.py
@@ -1,7 +1,7 @@
 def string_reverse(string):
-    reverse_string=""
+    reverse_string="" #Holds the reversed string
     for char in string:
-        reverse_string=""+char+""+reverse_string+""
+        reverse_string=""+char+""+reverse_string+"" #prefixes reverse_string with the character
     print(reverse_string)
 
 string=input("Enter a string: ")
